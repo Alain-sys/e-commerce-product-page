83a3e4bb9d969381dd354bd88d709861bfb96c82
Alain-sys <74666416+Alain-sys@users.noreply.github.com>
2022-08-15 22:24:30 +0200

    css: header on mobile design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,53 @@
 import React from 'react';
+import { useRef } from 'react';
 import './Header.css';
 import Cart from './Cart';
-import logo from '../../images/logo.svg';
-import avatar from '../../images/image-avatar.png';
+import iconHamburger from '../../images/icon-menu.svg';
+import iconCross from '../../images/icon-close.svg';
+import iconLogo from '../../images/logo.svg';
+import iconAvatar from '../../images/image-avatar.png';
 
 const Header = () => {
+  const menu = useRef(null);
+
   return (
     <header>
-      <div className="menu">
-        <img src={logo} alt="logo of our company written in lowercase with the black color " />
-        <nav>
-          <ul className="menu-list">
-            <li className="menu-list__items">Collections</li>
-            <li className="menu-list__items">Men</li>
-            <li className="menu-list__items">Women</li>
-            <li className="menu-list__items">About</li>
-            <li className="menu-list__items">Contact</li>
-          </ul>
-        </nav>
+      <div className="logo-hamburger">
+        <button
+          className="hamburger"
+          type="button"
+          onClick={() => {
+            menu.current.classList.toggle('active');
+          }}>
+          <img src={iconHamburger} alt="three grey horizontal bars" />
+        </button>
+        <img src={iconLogo} alt="logo of our company written in lowercase with the black color " />
       </div>
+      <nav
+        ref={menu}
+        className="menu"
+        onClick={() => {
+          menu.current.classList.toggle('active');
+        }}>
+        <ul className="menu__list" onClick={(e) => e.stopPropagation()}>
+          <button
+            className="menu__close"
+            type="button"
+            onClick={() => {
+              menu.current.classList.toggle('active');
+            }}>
+            <img src={iconCross} alt="grey cross" />
+          </button>
+          <li className="menu__list__items">Collections</li>
+          <li className="menu__list__items">Men</li>
+          <li className="menu__list__items">Women</li>
+          <li className="menu__list__items">About</li>
+          <li className="menu__list__items">Contact</li>
+        </ul>
+      </nav>
       <div className="user">
         <Cart />
-        <img className="user__avatar" src={avatar} alt="avatar for represent user connected" />
+        <img className="user__avatar" src={iconAvatar} alt="avatar for represent user connected" />
       </div>
     </header>
   );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ProductSlider from './ProductSlider';
+import ProductBuy from './ProductBuy';
 
 const Product = () => {
-  return <></>;
+  return (
+    <main className="product">
+      <ProductSlider />
+      <ProductBuy />
+    </main>
+  );
 };
 
 export default Product;
