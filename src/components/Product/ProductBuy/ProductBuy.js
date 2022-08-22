a1d5178540ce52d4986075d33d37a13e5d5c0ad2
Alain-sys <74666416+Alain-sys@users.noreply.github.com>
2022-08-22 22:43:52 +0200

    fix: svg

--- src/components/Product/ProductBuy/ProductBuy.js
+++ src/components/Product/ProductBuy/ProductBuy.js
@@ -31,7 +31,7 @@ const ProductBuy = ({ newProduct, setNewProduct }) => {
               d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z"
               id="a"
               fill="#FF7E1B"
-              fill-rule="nonzero"
+              fillRule="nonzero"
             />
           </svg>
         </button>
